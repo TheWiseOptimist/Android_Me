d17bc3f940dcf14731760e7ad458820eb99caa33
clyde <clyder@thewiseoptimist.net>
2018-09-14 16:06:28 -0700

    complete lesson 2:18 Exercise: Create MasterListFragment

--- app/src/main/java/com/example/android/android_me/ui/MasterListAdapter.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListAdapter.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.android_me.ui;
 
@@ -35,9 +35,10 @@ public class MasterListAdapter extends BaseAdapter {
 
     /**
      * Constructor method
+     *
      * @param imageIds The list of images to display
      */
-    public MasterListAdapter(Context context, List<Integer> imageIds) {
+    MasterListAdapter(Context context, List<Integer> imageIds) {
         mContext = context;
         mImageIds = imageIds;
     }
@@ -50,11 +51,17 @@ public class MasterListAdapter extends BaseAdapter {
         return mImageIds.size();
     }
 
+    /**
+     * Required but not used
+     */
     @Override
     public Object getItem(int i) {
         return null;
     }
 
+    /**
+     * Required but not used
+     */
     @Override
     public long getItemId(int i) {
         return 0;
