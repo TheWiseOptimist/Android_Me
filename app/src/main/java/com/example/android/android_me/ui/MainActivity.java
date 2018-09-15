77c3c30cdf75bd8bb3d7e86d95e3660797a938bb
clyde <clyder@thewiseoptimist.net>
2018-09-14 22:59:09 -0700

    partial - Lesson 2:24 Exercise: Two Pane Layout

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,24 +1,25 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.android_me.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,7 +28,7 @@ import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
 // Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
     // Variables to store the values for the list index of the selected images
     // The default value will be index = 0
@@ -36,8 +37,10 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
     private int legIndex;
 
     // TODO (3) Create a variable to track whether to display a two-pane or single-pane UI
-        // A single-pane display refers to phone screens, and two-pane to larger tablet screens
-
+    // A single-pane display refers to phone screens, and two-pane to larger tablet screens
+    private boolean isTablet = isTablet();
+    private Button nextButton;
+    private float smallestWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +49,30 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
         // TODO (4) If you are making a two-pane display, add new BodyPartFragments to create an initial Android-Me image
         // Also, for the two-pane display, get rid of the "Next" button in the master list fragment
+        getSmallestWidth();
 
+        nextButton = findViewById(R.id.next_button);
+        if (isTablet()) {
+            nextButton.setVisibility(View.GONE);
+        } else nextButton.setVisibility(View.VISIBLE);
+
+
+        Toast.makeText(this, "sw" + smallestWidth, Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isTablet() {
+        return smallestWidth >= 600;
+    }
+
+    private void getSmallestWidth() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int widthPixels = metrics.widthPixels;
+        int heightPixels = metrics.heightPixels;
+        float scaleFactor = metrics.density;
+        float widthDp = widthPixels / scaleFactor;
+        float heightDp = heightPixels / scaleFactor;
+        smallestWidth = Math.min(widthDp, heightDp);
     }
 
     // Define the behavior for onImageSelected
@@ -63,21 +89,25 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
         // bodyPartNumber will be = 0 for the head fragment, 1 for the body, and 2 for the leg fragment
         // Dividing by 12 gives us these integer values because each list of images resources has a size of 12
-        int bodyPartNumber = position /12;
+        int bodyPartNumber = position / 12;
 
         // Store the correct list index no matter where in the image list has been clicked
         // This ensures that the index will always be a value between 0-11
-        int listIndex = position - 12*bodyPartNumber;
+        int listIndex = position - 12 * bodyPartNumber;
 
         // Set the currently displayed item for the correct body part fragment
-        switch(bodyPartNumber) {
-            case 0: headIndex = listIndex;
+        switch (bodyPartNumber) {
+            case 0:
+                headIndex = listIndex;
                 break;
-            case 1: bodyIndex = listIndex;
+            case 1:
+                bodyIndex = listIndex;
                 break;
-            case 2: legIndex = listIndex;
+            case 2:
+                legIndex = listIndex;
                 break;
-            default: break;
+            default:
+                break;
         }
 
         // Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
@@ -91,13 +121,15 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         intent.putExtras(b);
 
         // The "Next" button launches a new AndroidMeActivity
-        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton = findViewById(R.id.next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent);
             }
         });
+        if (isTablet()) nextButton.setVisibility(View.GONE);
+        else nextButton.setVisibility(View.VISIBLE);
 
     }
 
